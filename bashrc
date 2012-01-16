7aae8d9129db0f40bee402de120ee3193abff375
garamonfok <francois@barrabin.org>
2012-01-16 09:28:19 +0100

    :P

---
+++ bashrc
@@ -0,0 +1,99 @@
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+#xmodmap /home/francisco/.emacs.d/ctrlcapswap
+# don't put duplicate lines in the history. See bash(1) for more options
+export HISTCONTROL=ignoredups
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+if [ `whoami` = 'root' ]; then
+    export PS1='${debian_chroot:+($debian_chroot)}\[\033[00;32m\]\H\[\033[00m\] \[\033[01;34m\]\w/\[\033[31m\]\$\[\033[00m\] '
+else
+    export PS1='${debian_chroot:+($debian_chroot)}\[\033[00;32m\]\H\[\033[00m\] \[\033[01;34m\]\w/\[\033[00m\]\$ '
+fi
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    echo -ne "\033]0;${HOSTNAME}: ${PWD/$HOME/~}\007"
+    ;;
+*)
+    ;;
+esac
+
+#echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"
+
+# enable color support of ls and also add handy aliases
+if [ "$TERM" != "dumb" ]; then
+    eval "`dircolors -b`"
+    alias ls='ls --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -lh'
+alias la='ls -A'
+alias lt='ls -ltr'
+alias ssh='ssh -X'
+alias l='ls -CF -1'
+alias g='egrep -i --color'
+alias j='jobs'
+alias h='head -n 25'
+alias grep="grep --color"
+alias t='tail -n 25'
+alias ssh='ssh -X'
+alias pl='perl'
+alias rm="rm -i" 
+alias mv="mv -i" 
+alias cp="cp -i" 
+
+
+## HISTORY
+export HISTSIZE=100000          # big big history
+
+# share same history in all root shells
+export PROMPT_COMMAND='history -a;history -n'
+shopt -s histappend
+
+# add date+time to commands in history
+HISTTIMEFORMAT="%m/%h - %H:%M:%S   "
+
+# queue system qstat: 
+#. /opt/sge/default/common/settings.sh
+if [[ -e /opt/sge/default/common/settings.sh ]]; then
+    . /opt/sge_settings.sh
+    alias qfinish="qstat -f -s prszh"
+    alias qall='qstat -u "*"'
+    alias qerror='qstat -explain E -j'
+
+    qtodo() { qstat -u `whoami` | grep " $1" | grep ' qw '| sed "s/.* //"| sed "s/:1//" | awk -F'-' '{print $2 - $1}'; }
+    qokupied() { qstat -u `whoami` -j $1 | grep usage | wc -l; }
+    qmean() { qacct -o `whoami` -j $1 | grep ru_wallclock | cut -d' ' -f2 | awk '{TOTAL+=$1} END{printf("doneJobs:_%d,_in_meanTime:_%dh_%dm_%ds_(%dsec) %d\n",NR,int((TOTAL/NR)/3600),((TOTAL/NR/3600)-int(TOTAL/NR/3600))*60,TOTAL/NR-(60*int(TOTAL/NR/60)),TOTAL/NR,TOTAL/NR)}' ;}
+    qrest() { qstat; echo `qmean $1` `qtodo $1` `qokupied $1` | awk '{REST = ($3)*($2)/($4)} END {printf( $1 "\nresting time %dd %dh %dm %ds (= %dsec = %djobs todo in %dsec/%dcpus)\n"),int(REST)/86400,int((REST)/3600)-(int(REST/86400)*24),((REST/3600)-int(REST/3600))*60,REST-(60*int(REST/60)),REST,$3,$2,$4}'| sed "s/_/ /g"; }
+
+    qalltodo() { qstat -u $1 | grep " $2" | grep ' qw '| sed "s/.* //"| sed "s/:1//" | awk -F'-' '{print $2 - $1}'; }
+    qallokupied() { qstat -u $1 -j $2 | grep usage | wc -l; }
+    qallmean() { qacct -o $1 -j $2 | grep ru_wallclock | cut -d' ' -f2 | awk '{TOTAL+=$1} END{printf("doneJobs:_%d,_in_meanTime:_%dh_%dm_%ds_(%dsec) %d\n",NR,int((TOTAL/NR)/3600),((TOTAL/NR/3600)-int(TOTAL/NR/3600))*60,TOTAL/NR-(60*int(TOTAL/NR/60)),TOTAL/NR,TOTAL/NR)}' ;}
+    qallrest() { qstat -u $1; echo `qallmean $1 $2` `qalltodo $1 $2` `qallokupied $1 $2` | awk '{REST = ($3)*($2)/($4)} END {printf( $1 "\nresting time %dd %dh %dm %ds (= %dsec = %djobs todo in %dsec/%dcpus)\n"),int(REST)/86400,int((REST)/3600)-(int(REST/86400)*24),((REST/3600)-int(REST/3600))*60,REST-(60*int(REST/60)),REST,$3,$2,$4}'| sed "s/_/ /g"; }
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+fi
+
---
+++ init.el
@@ -0,0 +1,89 @@
+(setq frame-title-format "%f")
+
+; load gabi things
+(load-file "~/.emacs.d/epy-init.el")
+(add-to-list 'load-path "~/.emacs.d/") ;; tell where to load the various files
+(require 'epy-setup) ;; It will setup other loads, it is required!
+(require 'epy-python) ;; If you want the python facilities [optional]
+(require 'epy-completion) ;; If you want the autocompletion settings [optional]
+(require 'epy-editing) ;; For configurations related to editing [optional]
+(require 'epy-bindings) ;; For my suggested keybindings [optional]
+
+(epy-setup-ipython)
+
+(global-linum-mode 0)
+(display-time)
+
+;;;; general
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(frame-background-mode (quote dark))
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+; '(pc-selection-mode t nil (pc-select)) ;; REMOVED BECAUSE OTHERWISE UP-DOWN ALT MOVEMENT OF SELECTION DO NO WORK
+ '(scroll-bar-mode nil)
+ '(setq visible-bell t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil nil (tool-bar))
+ '(truncate-lines t))
+
+(set-cursor-color "lightgrey")
+
+; key-bindings
+(global-unset-key "\M-g")
+(global-set-key "\M-g" 'goto-line)
+(global-set-key "\M-s" 'display-local-help)
+(global-set-key [\s-\tab] 'rope-code-assist)
+(global-set-key [f6] 'other-window)
+
+;somthing that uses run-python
+(defadvice py-shell (around py-shell-with-history)
+  (if (comint-check-proc "*Python*")
+      ad-do-it
+    (setq comint-input-ring-file-name
+          (if (string-equal py-python-command ipython-command)
+              (concat (or (getenv "IPYTHONDIR") "~/.ipython") "/history")
+            (or (getenv "PYTHONHISTORY") "~/.python-history.py")))
+    (comint-read-input-ring t)
+    (let ((buf (current-buffer)))
+      ad-do-it
+      (unless py-shell-initial-switch-buffers
+        (switch-to-buffer-other-window buf)))))
+(ad-activate 'py-shell)
+
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "grey85" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(flymake-errline ((((class color)) (:underline "red"))))
+ '(flymake-warnline ((((class color)) (:underline "yellow"))))
+ '(font-lock-builtin-face ((((class color) (min-colors 88) (background dark)) (:foreground "cyan"))))
+ '(font-lock-comment-face ((t (:foreground "grey50" :slant italic))))
+ '(font-lock-doc-string-face ((((class color) (background dark)) (:foreground "#00FA85" :bold t)) ((class color) (:foreground "darkGreen"))))
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background dark)) (:foreground "plum" :bold t))))
+ '(font-lock-reference-face ((((class color) (background dark)) (:foreground "#FAD987" :bold t)) ((class color) (:foreground "maroon4"))))
+ '(font-lock-string-face ((((class color) (min-colors 88) (background dark)) (:foreground "#00EA75"))))
+ '(paren-face-match ((((class color)) (:background "green" :foreground "black"))))
+ '(paren-face-mismatch ((((class color)) (:background "black" :foreground "red"))))
+ '(paren-face-no-match ((((class color)) (:background "red" :foreground "grey"))))
+ '(show-paren-match ((((class color)) (:background "green2"))))
+ '(show-paren-mismatch ((((class color)) (:background "red" :foreground "white")))))
+ 
+;;; perl replace
+(defun perl-replace (start end)
+  "Replace a text pattern in a  region using perl expressions"
+  (interactive "*r")
+  (setq regexp (read-string "Regexp: "))
+  (setq to-string (read-string (concat "[" regexp "] Replacement: ")))
+  (setq command (concat "perl -e 's/" regexp "/" to-string "/g' -p"))
+  (shell-command-on-region start end command nil 1)
+  )
+(global-set-key "\M-r" 'perl-replace)
